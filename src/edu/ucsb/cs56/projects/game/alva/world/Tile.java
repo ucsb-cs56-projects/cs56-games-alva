6f9b073f644c01b7c16e626b28adf709e5d5376c
Oscar <o_k_wang@umail.ucsb.edu>
2017-11-12 10:20:41 -0800

    changed Tile & TileType

--- src/edu/ucsb/cs56/projects/game/alva/world/Tile.java
+++ src/edu/ucsb/cs56/projects/game/alva/world/Tile.java
@@ -6,20 +6,29 @@ import java.io.Serializable;
 
 import edu.ucsb.cs56.projects.game.alva.Element;
 import edu.ucsb.cs56.projects.game.alva.Util.Vector2;
-
+/**Enables textures to drawn on the window through Tile objects 
+ */
 public class Tile extends Element implements Serializable {
 
     TileType type;
-
+	/**Uses the Element class constructor to create an instance of rectangle object to set bounds and make t an instance variable of Tile Class
+	 * @param x Vector2 object x position
+	 * @param y Vector2 object y position
+	 * @param t passes object of type TileType to be made into instance variable of Tile Class 
+	 */
 	public Tile(int x, int y, TileType t) {
 		super(new Vector2(x * 100, y * 100), 100, 100);
         type = t;
 	}
-	
+	/**Draw specific texture object on window
+	 * @param win Graphics2D object that represents the window object for drawing to a GUI
+	 */
 	public void drawIt(Graphics2D win) {
 		type.getTexture().draw(this, win);
 	}
-	
+	/**Gets the specific Tile Type 
+	 * @return the specific Tile Type
+	 */
 	public TileType getTileType() {
 		return type;
 	}
--- src/edu/ucsb/cs56/projects/game/alva/world/TileType.java
+++ src/edu/ucsb/cs56/projects/game/alva/world/TileType.java
@@ -13,14 +13,23 @@ public enum TileType {
           
     private final Texture t;
     private boolean isPassable;
+    /**Determines if a texture object is passable
+     * @param t passes object of type Texture 
+     * @param pass passes value of type boolean; if false not passable, if true passable
+     */
     private TileType(Texture t, boolean pass) {
             this.t = t;
             isPassable = pass;
     }
-    
+    /**Gets the texture object
+     * @return the texture object
+     */
     public Texture getTexture() {
         return t;
     }
+    /**gets the value of 'isPassable'; false or true
+     * @return the value of 'isPassable'; false or true
+     */
     public boolean isPassable() {
     	return isPassable;
     }
